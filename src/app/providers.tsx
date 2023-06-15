ea91f28e454f1ba6f29ee21ed8de91e75dd9fb15
Zushun Chen <zushun.chen@thoughtworks.com>
2023-06-15 13:40:14 +0800

    add analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,15 @@
 
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from '@vercel/analytics/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ChakraProvider>
+          {children}
+          <Analytics />
+      </ChakraProvider>
     </CacheProvider>
   );
 }

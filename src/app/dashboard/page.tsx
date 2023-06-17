e86ec6695bb30b7acbc15ffe69d20caeaedf1128
Siyuan Li <siyuan.li2@thoughtworks.com>
2023-06-17 15:22:56 +0800

    fix dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,35 +17,41 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import Navbar from "@/components/Navbar";
 import { Journey, JourneyFileParser } from "@/lib/JourneyFileParser";
 import UserJourney from "@/components/UserJourney";
-import { journey2 } from "@/data/journey2";
+import { prodJourneyExampleData } from "@/data/prod_journey_example";
 import { prompt } from "@/data/prompt";
 import { ChatMessage } from "@/types/chat";
 import { debounced } from "@/lib/debounce";
 
+const whoInputPlaceHolder = "I am a project manager.";
+const businessDomainPlaceHolder = "MQL、CRM、Salesforce, etc.";
+const wantToPlaceHolder =
+  "Digitize the MQL handover process based on the existing customized CRM to improve MQL conversion rates.";
+const keyBusinessPlaceHolder =
+  "1.Marketers identify MQLs and label them.\n2.Sales managers review and assign MQLs daily.\n3.Sales reps follow up with MQLs.\n4.Sales reps identify opportunities or return MQLs to marketing.";
+const painPointPlaceHolder =
+  "There are a lot of spam (unqualified) leads in MQLs; sales reps are unsure why a lead was identified as qualified by Marketers and how to follow-up; Marketers are unaware if sales reps have followed up with MQLs and what actions were taken.";
+
 export default function ChatPage() {
-  const [buttonHoverStyle, setbuttonHoverStyle] = useState({
+  // user inputs
+  const [whoInputValue, setWhoInputValue] = useState(whoInputPlaceHolder);
+  const [businessDomainInputValue, setBusinessDomainInputValue] = useState(
+    businessDomainPlaceHolder
+  );
+  const [wantToInputValue, setWantToInputValue] = useState(wantToPlaceHolder);
+  const [keyBusinessInputValue, setKeyBusinessInputValue] = useState(
+    keyBusinessPlaceHolder
+  );
+  const [painPointInputValue, setPainPointInputValue] =
+    useState(painPointPlaceHolder);
+
+  const [isLoading, setIsLoadingValue] = useState(false);
+  const [buttonHoverStyle, setButtonHoverStyle] = useState({
     bg: "#9054DF",
   });
-  const [isLoading, setIsLoadingValue] = useState(false);
-  const [whoInputValue, setWhoInputValue] = useState("");
-  const [businessDomainInputValue, setBusinessDomainInputValue] = useState("");
-  const [wantToInputValue, setWantToInputValue] = useState("");
-  const [keyBusinessInputValue, setKeyBusinessInputValue] = useState("");
-  const [painPointInputValue, setPainPointInputValue] = useState("");
-  const whoInputPlaceHolder = "I am a project manager.";
-  const businessDomainPlaceHolder = "MQL、CRM、Salesforce, etc.";
-  const wantToPlaceHolder =
-    "Digitize the MQL handover process based on the existing customized CRM to improve MQL conversion rates.";
-  const keyBusinessPlaceHolder =
-    "1.Marketers identify MQLs and label them.\n2.Sales managers review and assign MQLs daily.\n3.Sales reps follow up with MQLs.\n4.Sales reps identify opportunities or return MQLs to marketing.";
-  const painPointPlaceHolder =
-    "There are a lot of spam (unqualified) leads in MQLs; sales reps are unsure why a lead was identified as qualified by Marketers and how to follow-up; Marketers are unaware if sales reps have followed up with MQLs and what actions were taken.";
-  const [journey, setJourney] = useState<Journey>(
-    new JourneyFileParser(journey2).getJourney()
-  );
+
   const scrollOutputRef: any = useRef();
   const [journeyData, setJourneyData] = useState<Journey>(
-    new JourneyFileParser(journey2).getJourney()
+    new JourneyFileParser(prodJourneyExampleData).getJourney()
   );
   const [chatgptResponse, setChatgptResponse] = useState("");
   useEffect(() => {
@@ -55,7 +61,6 @@ export default function ChatPage() {
   const [lastChunkUpdateTime, setlastChunkUpdateTime] = useState(new Date());
 
   const handleGenerate = () => {
-    let value = painPointInputValue.trim();
     if (
       whoInputValue.trim() === "" ||
       businessDomainInputValue.trim() === "" ||
@@ -71,48 +76,27 @@ export default function ChatPage() {
       return;
     }
     startLoading();
-    const prompt = generatePrompt(
+    const userInputs = generateUserInputs(
       whoInputValue,
       businessDomainInputValue,
       wantToInputValue,
       keyBusinessInputValue,
       painPointInputValue
     );
-    //     const prompt = `我会给你一个需求，你要分析用户旅程中的stage和task，并按照下面的代码格式返回给我：
-    // header:
-    //   role: string
-    //   persona: string
-    //   scenario: string
-    //   goals: string
-    // stages:
-    //   - stage: string
-    //     tasks:
-    //       - task: string
-    //         touchpoint: string
-    //         emotion: number 1-3
-    //   - stage: string
-    //     tasks:
-    //       - task: string
-    //         touchpoint: string
-    //         emotion: number 1-3
-    //       - task: string
-    //         touchpoint: string
-    //         emotion: number 1-3
-    // `;
-    setJourney(new JourneyFileParser(value).getJourney());
+
     let messages: ChatMessage[] = [];
-    // messages.push({ role: "assistant", content: prompt });
-    messages.push({ role: "user", content: prompt });
+    messages.push({ role: "user", content: userInputs });
 
     callChatGPT(messages);
   };
-  const generatePrompt = (
-    whoInputValue,
-    businessDomainInputValue,
-    wantToInputValue,
-    keyBusinessInputValue,
-    painPointInputValue
+  const generateUserInputs = (
+    whoInputValue: string,
+    businessDomainInputValue: string,
+    wantToInputValue: string,
+    keyBusinessInputValue: string,
+    painPointInputValue: string
   ) => {
+    ``;
     prompt.replaceAll("{业务领域}", businessDomainInputValue);
     prompt.replaceAll("{角色}", whoInputValue);
     prompt.replaceAll("{目标}", wantToInputValue);
@@ -152,8 +136,7 @@ export default function ChatPage() {
       // stream update data
       updateJourneyData(responseMessage, false);
     }
-    // final update data
-    console.log(1);
+    // final update
     updateJourneyData(responseMessage, true);
     stopLoading();
   };
@@ -188,12 +171,12 @@ export default function ChatPage() {
   };
   const startLoading = () => {
     setIsLoadingValue(true);
-    setbuttonHoverStyle({ bg: "" });
+    setButtonHoverStyle({ bg: "" });
   };
 
   const stopLoading = () => {
     setIsLoadingValue(false);
-    setbuttonHoverStyle({ bg: "#9054DF" });
+    setButtonHoverStyle({ bg: "#9054DF" });
   };
 
   return (

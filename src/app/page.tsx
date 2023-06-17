e42231bab508dbe87e1e4dfad52e4fa1cc6b5217
Siyuan Li <siyuan.li2@thoughtworks.com>
2023-06-17 12:10:31 +0800

    add bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 // app/page.tsx
 "use client";
 
-import { Box, Center, Text, } from '@chakra-ui/react';
-import Navbar from '@/components/Navbar';
+import { Box, Center, Text } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
 
 export default function HomePage() {
   return (
     <Box h="100vh" w="100vw">
       <Navbar></Navbar>
-      <Center h="calc(100vh - 64px)" alignItems="center">
+      <Center
+        h="calc(100vh - 64px)"
+        alignItems="center"
+        className="gradient-card"
+      >
         <Text
-          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgGradient="linear(to-l, #FFF, #FFF)"
           bgClip="text"
-          fontSize="6xl"
+          fontSize="84px"
           fontWeight="extrabold"
         >
           Powerful User Journey AI Generator

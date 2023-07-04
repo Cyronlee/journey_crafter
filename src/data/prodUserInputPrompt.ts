d028453b4da2ec6f8c9a343e7a8797d2cc0d6e69
Siyuan Li <siyuan.li2@thoughtworks.com>
2023-07-04 17:02:22 +0800

    fix

--- src/data/prodUserInputPrompt.ts
+++ src/data/prodUserInputPrompt.ts
@@ -22,13 +22,13 @@ export const prodUserInputPrompt: string = `
 3. 用户旅程的Stages。用户旅程包含多个Stage，多个Stage之间有因果顺序。
 4. 用户旅程的每个Stage中包括 tasks，touchpoint和emotion。task是该关键角色在该Stage中要完成的业务活动。touchpoint是完成task时用户和服务的所有接触点（如工具）。emotion 是1到3的一个整数。在一个stage中，当用户的行为和触点非常复杂，需要大量输入和跳转触点的时候，emotion为1；在一个stage中，用户的行为和触点一般复杂，需要输入或者跳转触点的时候，emotion为2；在一个stage中，当用户只需点击和一个触点的时候，emotion为3。
 
-用户旅程的例子如下：
+给你一个例子：
 ===
 上下文：
 我们的客户是一个全球有10000人的软件咨询企业，客户希望我们可以针对Marketer角色优化当前在CRM系统中的MQL handover流程。CRM是一个基于salesforce以及客户业务定制的网页系统，包含了lead管理，contact管理，活动管理，campaign管理以及商机管理功能。用户的关键流程主要为目前CRM中已经存在MQL handover流程，关键流程主要为：Marketer手动识别MQL并标记MQL，方便销售经理每天浏览MQL并分配MQL，并让销售接收MQL并做跟进，Marketer接收销售退回的MQL进行分析。在这个流程中，Marketer希望能够及时准确地将MQL传递给销售，帮助销售加快MQL转化为商机。Marketer目前存在以下痛点：MQL中Spam比例过高，花费比较多的时间从系统中剔除Spam数据；Marketer不知道应该给销售传递什么信息来帮助销售做后续的跟进；3.Marketer不知道销售接收到MQL之后是否采取行动以及什么行动。根据我给你提供的Context，请作为一个领域MQL、CRM、Salesforce， etc.等专家和资深的产品经理，设计用户Marketer的TO-BE用户旅程，解决用户Marketer的痛点及领域难点，帮助用户Marketer更好的达到目标。
 问题：
 请返回最好的一个TO-BE用户旅程，并确保输出的TO-BE用户旅程中只涉及该用户。
-答案：
+输出：
 \`\`\`
 header:
   role: "Marketer"
@@ -61,7 +61,7 @@ stages:
 \`\`\`
 ===
 
-要求：请你分析最好的一个用户旅程，并严格按照下面的yaml格式返回给我，内容需要为英文。
+要求：请你分析最好的一个用户旅程，并严格按照下面的yaml格式返回给我，注意开头为begin，结尾为end，并且内容需要为英文。
 
 \`\`\`
 begin
